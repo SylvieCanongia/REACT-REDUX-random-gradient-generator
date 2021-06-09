7bab6cb1ebcf62abcccb95812c5225d051c67f1f
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2021-06-09 17:01:31 +0200

    RandomButtons component styles

--- src/components/RandomButtons/index.js
+++ src/components/RandomButtons/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './randomButtons.scss';
 
 const RandomButtons = () => (
-  <div>RandomButtons</div>
+  <div className="randomButtons buttons group">
+    <button type="button" className="button" id="randFirst">Random First</button>
+    <button type="button" className="button" id="randAll">Random All</button>
+    <button type="button" className="button" id="randLast">Random Last</button>
+  </div>
 );
 
 export default RandomButtons;
